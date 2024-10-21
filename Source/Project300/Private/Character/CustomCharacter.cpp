6e416a4fc4b366ac6f04e3f46ce46d1ceb8f48c8
Jake Smith <S00234729@atu.ie>
2024-10-21 21:46:45 +0100

    set up toggle

---
+++ Source/Project300/Private/Character/CustomCharacter.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CustomCharacter.h"
+
+// Sets default values
+ACustomCharacter::ACustomCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	isLockedOn = false;
+	targetHeightOffset = 20.0f;
+	lockedOnActor = nullptr;
+
+}
+
+// Called when the game starts or when spawned
+void ACustomCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ACustomCharacter::ToggleLockOn()
+{
+	if (isLockedOn)
+	{
+		isLockedOn = false;
+		lockedOnActor = nullptr;
+
+	}
+	else
+	{
+		if (lockOnCandidates.Num() > 0)
+		{
+			lockedOnActor = lockOnCandidates[0];
+
+			if (lockedOnActor)
+			{
+				isLockedOn = true;
+			}
+		}
+	}
+}
+
+// Called every frame
+void ACustomCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACustomCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
