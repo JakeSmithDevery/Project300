f432e4e97dfb9521ec7df6a19b7affd908c54932
Jake Smith <S00234729@atu.ie>
2024-10-21 20:35:13 +0100

    code started for lock on

---
+++ Source/Project300/Private/Character/CustomCharacter.cpp
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/CustomCharacter.h"
+
+// Sets default values
+ACustomCharacter::ACustomCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	isLockedOn = false;
+	targetHeightOffset = 20.0f;
+	lockedOnActor = nullptr;
+
+}
+
+// Called when the game starts or when spawned
+void ACustomCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACustomCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACustomCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Project300/Public/Character/CustomCharacter.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CustomCharacter.generated.h"
+
+UCLASS()
+class PROJECT300_API ACustomCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACustomCharacter();
+
+protected:
+
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	//functions
+	UFUNCTION(BlueprintCallable)
+	void ToggleLockOn();
+
+
+	//variables
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lock-On")
+	bool isLockedOn;
+
+	float targetHeightOffset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lock-On")
+	TArray<AActor*> lockOnCandidates;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Lock-On")
+	AActor* lockedOnActor;
+
+
+	
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
